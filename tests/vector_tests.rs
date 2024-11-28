3f10d12c680869fdd89cead2aa9ff2c85c4660c7
jakkie <jakubkielbasiewicz@gmail.com>
2024-11-28 22:50:25 +0100

    vector add

--- tests/vector_tests.rs
+++ tests/vector_tests.rs
@@ -11,4 +11,12 @@ mod tests {
         assert_eq!(empty, Vector::new(vec![]));
     }
 
+    #[test]
+    fn test_vector_add() {
+        let v1 = vector![1.0, 2.0, 3.0];
+        let v2 = vector![4.0, 5.0, 6.0];
+        let v3 = v1 + v2;
+        assert_eq!(v3, vector![5.0, 7.0, 9.0]);
+    }
+
 }
