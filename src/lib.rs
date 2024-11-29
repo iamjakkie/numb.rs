ea47e92e41fafa1356937bf25ff9321a1c605571
jakkie <jakubkielbasiewicz@gmail.com>
2024-11-29 16:24:08 +0100

    changed underlying structure to array

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,15 @@ pub use vector::Vector;
 
 #[macro_export]
 macro_rules! vector {
-    ($value:expr; $length:expr) => {
-        Vector::filled($value, $length)
+    ($value:expr; $size:expr) => {
+        Vector::<_, $size>::new([$value; $size])
     };
-    [] => {
-        Vector::<f64>::new(Vec::new())
-    };
-    ($($elem:expr),* $(,)?) => {
-        Vector::new(vec![$($elem),*])
-    };
+    ($($x:expr),+ $(,)?) => {{
+        const SIZE: usize = [$($x),*].len();
+        Vector::<_, SIZE>::new([$($x),*])
+    }};
+    () => {{
+        const SIZE: usize = 0;
+        Vector::<i32, SIZE>::new([])
+    }};
 }
--- src/vector.rs
+++ src/vector.rs
@@ -1,118 +1,140 @@
 use std::ops::{Add, Sub, Mul, Div};
 use num::{Num, Float, ToPrimitive};
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct Vector<T> {
-    elements: Vec<T>,
+#[derive(Debug, Clone, PartialEq, Copy)]
+pub struct Vector<T, const N: usize> {
+    pub elements: [T;N],
 }
 
-impl<T> Vector<T> 
+pub struct Scalar<U>(pub U);
+
+impl<U> From<U> for Scalar<U> {
+    fn from(value: U) -> Self {
+        Scalar(value)
+    }
+}
+
+impl<T, const N: usize> Vector<T, N> 
 where
     T: Num + Copy,
 {
-    pub fn new(elements: Vec<T>) -> Self {
+    pub fn new(elements: [T;N]) -> Self {
         Self { elements }
     }
 
-    pub fn zeros(length: usize) -> Self {
+    pub fn zeros() -> Self {
         Self {
-            elements: vec![T::zero(); length],
+            elements: [T::zero(); N],
         }
     }
 
-    pub fn ones(length: usize) -> Self {
+    pub fn ones() -> Self {
         Self {
-            elements: vec![T::one(); length],
+            elements: [T::one(); N],
         }
     }
 
-    pub fn filled(value: T, length: usize) -> Self {
+    pub fn filled(value: T) -> Self {
         Self {
-            elements: vec![value; length],
+            elements: [value; N],
         }
     }
 
     pub fn len(&self) -> usize {
-        self.elements.len()
+        N
     }
 }
 
-impl<T> Vector<T>
-where
-    T: Num + Copy + ToPrimitive,
-{
-    pub fn magnitude(&self) -> f64 {
-        self.elements
-            .iter()
-            .map(|&x| {
-                let x_f64 = x.to_f64().expect("Conversion to f64 failed");
-                x_f64 * x_f64
-            })
-            .sum::<f64>()
-            .sqrt()
-    }
-}
+// impl<T> Vector<T>
+// where
+//     T: Num + Copy + ToPrimitive,
+// {
+//     pub fn magnitude(&self) -> f64 {
+//         self.elements
+//             .iter()
+//             .map(|&x| {
+//                 let x_f64 = x.to_f64().expect("Conversion to f64 failed");
+//                 x_f64 * x_f64
+//             })
+//             .sum::<f64>()
+//             .sqrt()
+//     }
+// }
 
-impl<T> Add for Vector<T> 
-where
-    T: Num + Copy,
-{
-    type Output = Self;
+// impl<'a, T> Add for &'a Vector<T> 
+// where
+//     T: Num + Copy,
+// {
+//     type Output = Self;
 
-    fn add(self, other: Self) -> Self {
-        let elements = self.elements.iter().zip(other.elements.iter())
-            .map(|(a, b)| *a + *b)
-            .collect();
-        Self::new(elements)
-    }
-}
+//     fn add(self, other: Self) -> Self {
+//         let elements = self.elements.iter().zip(other.elements.iter())
+//             .map(|(a, b)| *a + *b)
+//             .collect();
+//         Self::new(elements)
+//     }
+// }
 
 
-impl<T> Sub for Vector<T> 
-where
-    T: Num + Copy,
-{
-    type Output = Self;
+// impl<T> Sub for Vector<T> 
+// where
+//     T: Num + Copy,
+// {
+//     type Output = Self;
 
-    fn sub(self, other: Self) -> Self {
-        assert_eq!(self.len(), other.len(), "Vectors must have the same length");
-        let elements = self
-            .elements
-            .iter()
-            .zip(other.elements.iter())
-            .map(|(a, b)| *a - *b)
-            .collect();
-        Self::new(elements)
-    }
-}
+//     fn sub(self, other: Self) -> Self {
+//         assert_eq!(self.len(), other.len(), "Vectors must have the same length");
+//         let elements = self
+//             .elements
+//             .iter()
+//             .zip(other.elements.iter())
+//             .map(|(a, b)| *a - *b)
+//             .collect();
+//         Self::new(elements)
+//     }
+// }
 
-impl<T, U> Mul<U> for Vector<T>
-where
-    T: Num + Copy + From<U>, 
-    U: Num + Copy,           
-{
-    type Output = Self;
+// impl<T, U> Mul<U> for Vector<T>
+// where
+//     T: Num + Copy + From<U>, 
+//     U: Num + Copy,           
+// {
+//     type Output = Self;
 
-    fn mul(self, scalar: U) -> Self {
-        let scalar_t = T::from(scalar); // Convert scalar to vector's type
-        let elements = self.elements.iter().map(|&x| x * scalar_t).collect();
-        Self::new(elements)
-    }
-}
+//     fn mul(self, scalar: U) -> Self {
+//         let scalar_t = T::from(scalar); // Convert scalar to vector's type
+//         let elements = self.elements.iter().map(|&x| x * scalar_t).collect();
+//         Self::new(elements)
+//     }
+// }
 
-impl<T, U> Mul<Vector<U>> for Vector<T>
-where
-    T: Num + Copy + From<U>, 
-    U: Num + Copy,
-{
-    type Output = T;
+// impl<T, U> Mul<Vector<U>> for Vector<T>
+// where
+//     T: Num + Copy + From<U>, 
+//     U: Num + Copy,
+// {
+//     type Output = T;
 
-    fn mul(self, other: Vector<U>) -> T {
-        assert_eq!(self.len(), other.len(), "Vectors must have the same length");
-        self.elements
-            .iter()
-            .zip(other.elements.iter())
-            .map(|(&a, &b)| a * T::from(b)) // Convert `b` to type `T`
-            .fold(T::zero(), |acc, x| acc + x) // Sum the results
-    }
-}
+//     fn mul(self, other: Vector<U>) -> T {
+//         assert_eq!(self.len(), other.len(), "Vectors must have the same length");
+//         self.elements
+//             .iter()
+//             .zip(other.elements.iter())
+//             .map(|(&a, &b)| a * T::from(b)) // Convert `b` to type `T`
+//             .fold(T::zero(), |acc, x| acc + x) // Sum the results
+//     }
+// }
+
+// impl<T, U> Mul<Vector<T>> for Scalar<U>
+// where
+//     T: Num + Copy + From<U>, 
+//     U: Num + Copy,
+// {
+//     type Output = Vector<T>;
+
+//     fn mul(self, vector: Vector<T>) -> Vector<T> {
+//         let scalar_t = T::from(self.0); // Convert scalar to vector's type
+//         let elements = vector.elements.iter().map(|&x| scalar_t * x).collect();
+//         Vector::new(elements)
+//     }
+// }
