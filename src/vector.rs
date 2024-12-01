a09b88e3a65b02622a6fb6d83ee4378c3890367e
jakkie <jakubkielbasiewicz@gmail.com>
2024-12-01 10:27:55 +0100

    cleanup

--- src/vector.rs
+++ src/vector.rs
@@ -1,5 +1,5 @@
 use std::ops::{Add, Sub, Mul};
-use num::{Num, Float, ToPrimitive};
+use num::{Num, ToPrimitive};
 use std::fmt::Debug;
 
 #[derive(Debug, Clone, PartialEq, Copy)]
@@ -187,48 +187,3 @@ where
         Vector::new(elements)
     }
 }
-
-// impl<T, U> Mul<U> for Vector<T>
-// where
-//     T: Num + Copy + From<U>, 
-//     U: Num + Copy,           
-// {
-//     type Output = Self;
-
-//     fn mul(self, scalar: U) -> Self {
-//         let scalar_t = T::from(scalar); // Convert scalar to vector's type
-//         let elements = self.elements.iter().map(|&x| x * scalar_t).collect();
-//         Self::new(elements)
-//     }
-// }
-
-// impl<T, U> Mul<Vector<U>> for Vector<T>
-// where
-//     T: Num + Copy + From<U>, 
-//     U: Num + Copy,
-// {
-//     type Output = T;
-
-//     fn mul(self, other: Vector<U>) -> T {
-//         assert_eq!(self.len(), other.len(), "Vectors must have the same length");
-//         self.elements
-//             .iter()
-//             .zip(other.elements.iter())
-//             .map(|(&a, &b)| a * T::from(b)) // Convert `b` to type `T`
-//             .fold(T::zero(), |acc, x| acc + x) // Sum the results
-//     }
-// }
-
-// impl<T, U> Mul<Vector<T>> for Scalar<U>
-// where
-//     T: Num + Copy + From<U>, 
-//     U: Num + Copy,
-// {
-//     type Output = Vector<T>;
-
-//     fn mul(self, vector: Vector<T>) -> Vector<T> {
-//         let scalar_t = T::from(self.0); // Convert scalar to vector's type
-//         let elements = vector.elements.iter().map(|&x| scalar_t * x).collect();
-//         Vector::new(elements)
-//     }
-// }
